5766fedb1fdfa1a56af670b3b99c28fb6de0dec7
crazypatoto <crazypatoto@gmail.com>
2020-07-07 16:58:13 +0800

    Adding jobs.json

---
+++ js/ui.js
@@ -0,0 +1,6 @@
+//M.AutoInit();
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems);
+  });
