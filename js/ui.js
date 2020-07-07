21185824db4d929152e64fd6255f39e4e5529ae1
crazypatoto <crazypatoto@gmail.com>
2020-07-07 18:53:53 +0800

    test

--- js/ui.js
+++ js/ui.js
@@ -1,6 +1,23 @@
 //M.AutoInit();
 
-document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('select');
-    var instances = M.FormSelect.init(elems);
+// document.addEventListener('DOMContentLoaded', function() {
+//     var elems = document.querySelectorAll('select');
+//     var instances = M.FormSelect.init(elems);
+//   });
+
+$(document).ready(function () {
+  $('select').formSelect();  //init selects
+
+  $.getJSON("jsondata/jobs.json", function (jobs) {
+
+    var jobSelect = $("#job-title-select");
+
+    $.each(jobs, function(index, job) {
+			jobSelect.append(`<option value=${job['id']}>${job['job_title']}</option>`);
+		});    
+
+    jobSelect.formSelect();  //init selects
   });
+
+
+});
